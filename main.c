c5f5e48e100b0c89dedc911c24363805ca057c57
alaahamed1 <alaa.elzeftawy@gmail.com>
2024-03-31 23:12:34 +0200

    initial commit

--- main.c
+++ main.c
@@ -2,20 +2,37 @@
 
 int main(int argc, char **argv)
 {
+	char buffer[1024];
 	unsigned int line_number = 1;
-	stack_t *stack = NULL;
+	stack_t *my_stack = NULL;
+	FILE *file_ptr;
 
 	if (argc != 2)
+	{
 		exit_usagefailure();
-
-	FILE *file_ptr;
+	}
 	file_ptr = fopen(argv[1], "r");
-	if (file_ptr == NULL)
+	if(file_ptr == NULL)
 	{
 		perror("Error opening file");
 		return (1);
 	}
-	fclose(file_ptr);
-	free_stack(stack);
-	return (EXIT_SUCCESS);
+	while(fgets(buffer, 1024, file_ptr) != NULL)
+	{
+		char *opcode = strtok(buffer, " \t\n");
+
+		if(opcode == NULL)
+		{
+			continue;
+		}
+		if(strcmp(opcode, "push") == 0)
+		{
+			push(&my_stack, line_number);
+		}
+		else if (strcmp(opcode, "pall") == 0)
+		{
+			pall(&my_stack);
+		}
+	}
+	return (0);
 }
--- push.c
+++ push.c
@@ -3,17 +3,18 @@
 void push(stack_t **stack, unsigned int line_number)
 {
 	stack_t *new_el;
+	char buffer[1024];
 
 	new_el = malloc(sizeof(stack_t));
-	if (new_el == NULL)
-	{
+	if (new_el == NULL) {
 		fprintf(stderr, "Error: Can't allocate memory at line %u\n", line_number);
 		exit(EXIT_FAILURE);
 	}
 
-	new_el->n = 10;
+	fgets(buffer, 1024, stdin);
+	sscanf(buffer, "%*s %d", &new_el->n);
+
 	new_el->prev = NULL;
 	new_el->next = *stack;
-
 	*stack = new_el;
 }
